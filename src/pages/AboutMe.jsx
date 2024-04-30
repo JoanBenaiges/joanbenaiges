76472beae074e934ee2afa890f4ec60ff27d0597
Joan Benaiges <jobeto87@gmail.com>
2024-04-30 15:32:23 +0100

    json done

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,16 +4,7 @@ function About() {
 
     return (
         <div className="aboutme-page">
-            <label class="switch">
-                <input type="checkbox" checked="checked" ></input>
-                <div class="button">
-                    <div class="light"></div>
-                    <div class="dots"></div>
-                    <div class="characters"></div>
-                    <div class="shine"></div>
-                    <div class="shadow"></div>
-                </div>
-            </label>
+
 
         </div>
 
